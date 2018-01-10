8bdc65550af10095f619cea11566ad9add94fe03
jennianelson <jennifernelson118@gmail.com>
2018-01-10 17:15:31 -0600

    added Facebook Omniauth and ability to add new subjects

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -5,4 +5,20 @@ class SubjectsController < ApplicationController
     def show
         @subject = Subject.find(params[:id])
     end
+
+    def new
+        @subject = Subject.new
+    end
+
+    def create
+        subject = current_user.subjects.build(subject_params)
+        subject.save
+        redirect_to root_path
+    end
+
+    private
+
+    def subject_params
+        params.require(:subject).permit(:title)
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,15 @@
 class User < ApplicationRecord
+  has_many :subjects
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :trackable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-  
-  has_many :subjects
+         :recoverable, :rememberable, :validatable,
+         :omniauthable, :omniauth_providers => [:facebook]
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+    end
+  end
 end
