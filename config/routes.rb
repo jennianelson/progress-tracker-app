af2357aa27f50a57f9a3f6330b1ed799ffa902a0
jennianelson <jennifernelson118@gmail.com>
2018-01-31 16:26:08 -0600

    Cleaned up routes to focus only on student user role

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,26 @@
 Rails.application.routes.draw do
-  resources :skills
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
+  #STUDENT USER
   root 'student_subjects#index'
   
-  resources :subjects
-      # resources :sections, only: [:new, :create, :edit, :update, :destroy]
-
-  resources :student_subjects do
+  resources :student_subjects, only: [:show, :destroy] do
     resources :sections, only: [:show]
   end
 
-  resources :student_standards, only:[:create, :edit, :update]
-  
+  resources :student_standards, only:[:edit, :update]
 
-  resources :sections, only: [:show] do
-    resources :standards, only:[:create, :index]
-  end
+
+  #ADMIN
+  # resources :subjects
+  # resources :sections, only: [:new, :create, :edit, :update, :destroy]
+
+  # resources :sections, only: [:show] do
+  #   resources :standards, only:[:create, :index]
+  # end
 
   # resources :standards, only: [:index]
+
 
 end
