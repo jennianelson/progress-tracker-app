d4f636c6145e72f44c232c0f8f41a7a5ad49202f
jennianelson <jennifernelson118@gmail.com>
2018-02-14 10:15:55 -0600

    Added custom message to student_subject validation

--- app/models/student_subject.rb
+++ app/models/student_subject.rb
@@ -1,7 +1,5 @@
 class StudentSubject < ApplicationRecord
     belongs_to :user 
     belongs_to :subject
-    # validates :subject_id, uniqueness: {message: "has already been added"}
-    validates_uniqueness_of :subject_id, :scope => :user_id
-
+    validates_uniqueness_of :subject_id, :scope => :user_id, :message => "has already been added"
 end
