2c9e28c66020a651659fd9b9da2649cc2dcf4793
jennianelson <jennifernelson118@gmail.com>
2018-02-14 10:10:56 -0600

    Fixed student_subject validations by adding scope

--- app/models/student_subject.rb
+++ app/models/student_subject.rb
@@ -1,6 +1,7 @@
 class StudentSubject < ApplicationRecord
     belongs_to :user 
     belongs_to :subject
-    validates :subject_id, uniqueness: {message: "has already been added"}
+    # validates :subject_id, uniqueness: {message: "has already been added"}
+    validates_uniqueness_of :subject_id, :scope => :user_id
 
 end
