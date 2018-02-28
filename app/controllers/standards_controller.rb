d11b21aaebf9bb877a7584744d48ebb3ddd305cf
jennianelson <jennifernelson118@gmail.com>
2018-02-28 16:52:25 -0600

    Admin can edit standard description from subject show page

--- app/controllers/standards_controller.rb
+++ app/controllers/standards_controller.rb
@@ -31,7 +31,7 @@ class StandardsController < ApplicationController
     def update
         @standard = Standard.find(params[:id])
         if @standard.update(standard_params)
-            redirect_to section_path(@standard.section)
+            redirect_to subject_path(@standard.subject)
         else
             render :edit
         end
@@ -43,6 +43,6 @@ class StandardsController < ApplicationController
     private
 
     def standard_params
-        params.require(:standard).permit(standards_attributes: [:include, :description, :dot_notation, :section_id, :subject_id])
+        params.require(:standard).permit(:description, standards_attributes: [:include, :description, :dot_notation, :section_id, :subject_id])
     end
 end
--- app/helpers/standards_helper.rb
+++ app/helpers/standards_helper.rb
@@ -8,13 +8,7 @@ module StandardsHelper
         standard.subject.title
     end
 
-    # def display_standard_text(a)
-    #     if a["description"] && a["statementLabel"] != "Disciplinary Core Idea" && a["statementLabel"] != "Domain" 
-    #         if a["comments"]
-    #             puts "#{a["description"]}"+ "#{a["comments"]}"
-    #         else
-    #             a["description"]
-    #         end
-    #     end
-    # end
+    def sort_standards(standards)
+        standards.sort_by {|s| s.dot_notation}
+    end
 end
