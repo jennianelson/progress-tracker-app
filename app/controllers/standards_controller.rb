d913d93aba5e14ce72077451e44d6fd4bdefc0bd
jennianelson <jennifernelson118@gmail.com>
2018-03-21 12:30:13 -0500

    Took out standards#new & standards#create since this is happening in subject/section#edit now

--- app/controllers/standards_controller.rb
+++ app/controllers/standards_controller.rb
@@ -1,29 +1,11 @@
 class StandardsController < ApplicationController
-
-    def new
-        @subject = Subject.find(params[:subject_id])
-        @standards_array = get_standards_array(parse_api("standard_sets", @subject.set_id))
-        @standard = Standard.new
-        @sections = @subject.sections
-    end
-
-    def create
-        @subject = Subject.find(standard_params["standards_attributes"]["0"]["subject_id"])
-        @standard = Standard.new(standard_params)
-        if @subject.standards.present?
-            redirect_to subject_path(@subject)
-        else
-            redirect_to new_standard_path, alert: "Unable to add standards."
-        end
-    end
+    before_action :set_standard
 
     def edit
-        @standard = Standard.find(params[:id])
         @subject = @standard.subject
     end
 
     def update
-        @standard = Standard.find(params[:id])
         if @standard.update(standard_params)
             redirect_to subject_path(@standard.subject)
         else
@@ -32,7 +14,6 @@ class StandardsController < ApplicationController
     end
 
     def destroy
-        @standard = Standard.find(params[:id])
         @standard.destroy
         redirect_to subject_path(@standard.subject)
     end
@@ -42,4 +23,9 @@ class StandardsController < ApplicationController
     def standard_params
         params.require(:standard).permit(:description, :dot_notation, :section_id, :asn_id, standards_attributes: [:include, :description, :dot_notation, :section_id, :subject_id, :asn_id])
     end
+
+    def set_standard
+        @standard = Standard.find(params[:id])
+    end
+
 end
