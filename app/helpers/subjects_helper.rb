685b5b50b72aa447853afcf70a5199848bc44199
jennianelson <jennifernelson118@gmail.com>
2018-03-22 07:54:12 -0500

    Put subject being used alert on subject#show as well as subject#edit

--- app/helpers/subjects_helper.rb
+++ app/helpers/subjects_helper.rb
@@ -2,7 +2,7 @@ module SubjectsHelper
 
     def subject_being_used_alert(subject)
         if subject.student_subjects.present?
-            "This subject is being used by #{subject.student_subjects.size} student(s). Deleting the subject or any of its sections will also delete the standards and student progress associated with it."
+            "**This subject is being used by #{subject.student_subjects.size} student(s). Deleting the subject or any of its sections will also delete the standards and student progress associated with it.**"
         end
     end
     
