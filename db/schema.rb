ae744f9c7c5818d1a9489c59c53afb1a67b44320
jennianelson <jennifernelson118@gmail.com>
2018-03-25 13:21:26 -0500

    Changed username to name in User table and added name field to sign up form

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180315220252) do
+ActiveRecord::Schema.define(version: 20180325180025) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,7 +59,7 @@ ActiveRecord::Schema.define(version: 20180315220252) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "username"
+    t.string "name"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
