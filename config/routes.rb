491f16c006f6deb48309411f7fc5ab8fe701d85a
jennianelson <jennifernelson118@gmail.com>
2018-03-25 13:54:29 -0500

    Created custom registrations controller for devise to add name attribute to params

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   #AUTHENTICATION
-  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  devise_for :users, :controllers => { registrations: 'registrations', omniauth_callbacks: "users/omniauth_callbacks" }
 
   root 'student_subjects#index'
 
