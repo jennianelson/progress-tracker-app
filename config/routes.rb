300a9fb342dc0313d740c5848ccc7f87ae4e3146
jennianelson <jennifernelson118@gmail.com>
2018-03-25 20:16:17 -0500

    Creating an admin scope for routes doesn't work because some controllers are used for mutliple roles. Deleted scope.

--- config/routes.rb
+++ config/routes.rb
@@ -4,21 +4,18 @@ Rails.application.routes.draw do
 
   root 'student_subjects#index'
 
-  resources :student_subjects, only: [:index, :show, :create] do 
+  resources :student_subjects, only: [:index, :show, :create, :destroy] do 
     resources :sections, only: [:show]
     resources :student_standards, only: [:index]
   end
   
   resources :student_standards, only:[:show, :edit, :update]
   
-  scope 'admin' do
-    delete 'student_subjects/:id', to: 'student_subjects#destroy'
-    resources :sections, only: [:edit, :update]
-    resources :standards, only: [:create, :update, :destroy]
-    resources :users, only: [:index, :show, :update, :destroy] 
-    resources :subjects do 
-        resources :standards, only: [:show, :new, :edit]
-    end
+  resources :sections, only: [:edit, :update]
+  resources :standards, only: [:create, :update, :destroy]
+  resources :users, only: [:index, :show, :update, :destroy] 
+  resources :subjects do 
+      resources :standards, only: [:show, :new, :edit]
   end
 
 end
